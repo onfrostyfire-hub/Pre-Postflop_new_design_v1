4d0da25c3e8cf2c07ee23ee6bbb6280cfb1aaebe
onfrostyfire-hub <onfrostyfire@gmail.com>
2026-03-15 23:35:21 -0300

    Update stats.py

--- views/stats.py
+++ views/stats.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
-import utils
+import poker_utils as utils
 
 def show():
     st.markdown("## 📊 Statistics Hub")
